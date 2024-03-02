70be1e9a851c3879c8891c45cf490f2fabc05aaf
valdmntina <duquesakura@gmail.com>
2024-03-02 18:13:13 -0500

    por si las

---
+++ Portafolio/Portafolio/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.inicio, name="inicio"),
+    path("portafolio/", views.portafolio, name="portafolio")
+]
